f66b369a313e21cb30dafb101c6d4e804b2b59bc
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-10-01 23:17:15 +0800

    16. Trigger Animation in Code
    GetComponent<Animator>().SetBool("Attack", false);
            GetComponent<Animator>().SetTrigger("Move");

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -52,11 +52,14 @@ public class EnemyAI : MonoBehaviour
     }
     void ChaseTarget()
     {
+        GetComponent<Animator>().SetBool("Attack", false);
+        GetComponent<Animator>().SetTrigger("Move");
         navMeshAgent.SetDestination(target.position);
     }
 
     void AttackTarget()
     {
+        GetComponent<Animator>().SetBool("Attack", true);
         Debug.Log("Chomp.. Chomp.. Chomp..");
     } 
     
