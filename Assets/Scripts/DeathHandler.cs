26f8e846d3f446b65c3bdf152902d106feb5b480
Patrick Joseph Pronuevo <3xjoseph@gmail.com>
2024-10-04 13:59:04 +0800

    31. Player Input to Select Weapon
    Created 2 Methods:
    
    ProcessKeyInput();
    ProcessScrollWheel();

--- Assets/Scripts/DeathHandler.cs
+++ Assets/Scripts/DeathHandler.cs
@@ -18,6 +18,7 @@ public class DeathHandler : MonoBehaviour
         GetComponent<StarterAssets.FirstPersonController>().enabled = false;
         gameOverCanvas.enabled = true;
         Time.timeScale = 0;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -2,15 +2,61 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 
 public class WeaponSwitcher : MonoBehaviour
 {
-    [SerializeField] int currentWeapon = 0;
+    [Tooltip("The index of the current weapon")][SerializeField] int currentWeapon = 0;
     void Start()
     {
         SetWeaponActive();
     }
 
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScrollWheel();
+
+        if (previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) currentWeapon = 0;
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) currentWeapon = 1;
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) currentWeapon = 2;
+        
+    }
+    void ProcessScrollWheel()
+    {
+        if ( Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else 
+            {
+                currentWeapon ++;
+            }
+        }
+        else if ( Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (currentWeapon <= 0)
+            {
+                currentWeapon = transform.childCount - 1;
+            }
+            else 
+            {
+                currentWeapon --;
+            }
+        }
+    }
     void SetWeaponActive()
     {
         int weaponIndex = 0;
@@ -27,11 +73,5 @@ public class WeaponSwitcher : MonoBehaviour
             }
             weaponIndex ++;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
